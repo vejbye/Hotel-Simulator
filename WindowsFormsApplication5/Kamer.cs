31a4a76669d8d17f11c78290d96f1f0902ff25c5
vejbye <ericvejbye@gmail.com>
2016-09-22 12:20:08 +0200

    classes aangemaakt

---
+++ WindowsFormsApplication5/Kamer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication5
+{
+    class Kamer
+    {
+        bool isBezet { get; set; }
+        int kamernr { get; set; }
+    }
+}
