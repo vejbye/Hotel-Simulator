181c8ea54f0e19bb489defd1b094cf5daabdf018
Calvin Kun <15020606@student.hhs.nl>
2016-09-28 13:44:08 +0200

    Got new classes

---
+++ WindowsFormsApplication5/Object/Guest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelSimulator.Object
+{
+    class Guest: GameObject
+    {
+        Room room { get; set; }
+
+    }
+}
